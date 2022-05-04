de4d3da2d4ad2e996b9912851623d90306cd3e2c
jackiem2 <jacqueline.merlan@gmail.com>
2022-05-03 23:23:19 -0500

    set up graphql

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,8 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql``;
+
+// export the typeDefs
+module.exports = typeDefs;
