17e3a1a00cf182b3b21d784f2597a29a81121d38
EmanSL <emanuelsilvalopez@gmail.com>
2022-05-05 03:49:13 -0400

    added models for user food and orders, NEEDS REVIEW

---
+++ server/models/User.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// import schema from Book.js
+const orderSchema = require('./Order');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    // set savedBooks to be an array of data that adheres to the bookSchema
+    orderHistory: [orderSchema],
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+// when we query a user, we'll also get another field that hopefully shows order history
+userSchema.virtual('orderHistory').get(function () {
+  return this.orderHistory.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
