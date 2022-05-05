d2637bf3044802639c88296eaf444a952538e310
jackiem2 <jacqueline.merlan@gmail.com>
2022-05-04 19:03:01 -0500

    connected mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,3 +1,10 @@
 const mongooose = require('mongoose');
 
-mongooose.connect(process.env.MONGODB_URI ||)
+mongooose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+module.exports = mongoose.connection;
