d288b1f15287335c03d7e84c0e28286ee8569753
Xallver <jamesstanley11@gmail.com>
2022-05-07 12:54:55 -0400

    created login portion of Mutation inside resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,6 +10,25 @@ const resolvers = {
     //         }
     //     }
     // }
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOneJ({ email });
+            // check if user exists with email and credentials
+            if (!user) {
+                throw new AuthenticationError("Incorrect credentials");
+            }
+            const correctPassword = await user.isCorrectPassword(password);
+
+            // check password
+            if (!correctPassword) {
+                throw new AuthenticationError("Incorrect credentials");
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+    }
 }
 
 module.exports = resolvers;
