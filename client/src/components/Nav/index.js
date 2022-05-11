ab5a110d781f4527e9222a71147b1048d02d7cab
jackiem2 <jacqueline.merlan@gmail.com>
2022-05-10 20:09:23 -0500

    removed unwanted files

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -12,7 +12,6 @@ function Nav() {
                 <ul className="flex-row">
                     <li><Link to="/menu">Menu</Link></li>
                     <li><Link to="/about">About</Link></li>
-                    <li><Link to="/staff">Staff</Link></li>
                     <li><Link to="/login">Login</Link></li>
                     <li><Link to="/cart">Cart</Link></li>
                 </ul>
