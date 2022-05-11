9c543a69df0b28f65d30d7f361ca1afaee457271
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-05-10 18:51:22 -0700

    added mutations/queries

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,27 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+    mutation Mutation($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+            email
+        }
+        }
+    }
+`;
