14692bdaa46bf267b74537ef6f00ff268fbd2e17
euc1 <jamesharrington585@hotmail.com>
2022-05-11 20:20:54 -0400

    setting up mongodb for heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/grub-burger', {});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/grub-burger',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 
 module.exports = mongoose.connection;
